50e10dff96425f66d89bd559cb74207765799528
MagmaOverlord <tomas.brejl@gmail.com>
2020-01-29 03:07:53 +0000

    Added a variable page

--- main.py
+++ main.py
@@ -3,12 +3,18 @@ app = flask.Flask(__name__)
 
 @app.route('/')
 def home():
-  return flask.send_file('static/index.html')  
+  return flask.send_file('static/index.html') 
 
 
 @app.route('/about')
 def about():
   return flask.send_file('static/about.html')
+
+@app.route('/iceCream/<num>')
+def iceCreamNumber(num):
+    output = "You chose "+num+" Mint Chocolate Chip Ice Creams, which is more than " +str(int(num)-1)
+    output += "<br/><a href = '/'>Home</a>";
+    return output
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8080)
